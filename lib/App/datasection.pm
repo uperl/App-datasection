339f644687ed5cb5d84fd6fb61551bb5b8de64ed
Graham Ollis <plicease@cpan.org>
2024-12-21 10:08:37 -0700

    Initial structure

---
+++ lib/App/datasection.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package App::datasection {
+
+  # ABSTRACT: Work with __DATA__ section files from the command line
+}
+
+1;
+
